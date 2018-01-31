4ce475b5bf665918c3a0187f37e687db3002b3b2
botfbmessenger <35079360+botfbmessenger@users.noreply.github.com>
2018-01-31 14:23:57 -0500

    Updating custom search code

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import urllib
 import json
 import os
 import re
+import pprint
 
 
 from flask import Flask
 from flask import request
 from flask import make_response
 from datetime import datetime as DateTime, timedelta as TimeDelta
-from googleapiclient.discovery import build, pprint
+from googleapiclient.discovery import build
 
 
 # Flask app should start in global layout
@@ -47,7 +48,11 @@ def makeWebhookResult(req):
         #cat = parameters.get("catalog-category")
         if ((req.get("originalRequest") is not None) and (req.get("originalRequest").get("source") == "facebook")):
             
+            my_api_key = "Google API key"
+            my_cse_id = "Custom Search Engine ID"
+            
             results = google_search('www.lanebryant.com', my_api_key, my_cse_id, num=10)
+            
             for result in results:
                 pprint.pprint(result)
                 
@@ -523,12 +528,10 @@ def getOrderStatusResponse(status, date):
             speech = "Sorry! I could not find that order. Please check the order number or zipcode and try again."    
     return speech
 
-my_api_key = "Google API key"
-my_cse_id = "Custom Search Engine ID"
-
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    print("Search Result: ", res)
     return res['items']
 
 if __name__ == '__main__':
