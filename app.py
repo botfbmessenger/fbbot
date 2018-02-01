e9113aa5975068c8870938ac22bb16476cc30a2b
botfbmessenger <35079360+botfbmessenger@users.noreply.github.com>
2018-02-01 10:18:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,6 @@ def makeWebhookResult(req):
                                         {
                                             "type":"web_url",
                                             "url":"https://peterssendreceiveapp.ngrok.io/view?item=103",
-                                            "messenger_extensions":true,
                                             "webview_height_ratio":"tall",
                                             "fallback_url":"https://peterssendreceiveapp.ngrok.io/"
                                         },
